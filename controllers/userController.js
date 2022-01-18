6937967029df63f5e4eb42ad8e77b23662e8b1a1
zakaria2105 <zelar903@gmail.com>
2022-01-18 22:33:38 +0100

    FSJS-68 hashing password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs")
 
 const userController = {};
 
 userController.register = async (req, res) => {
-  const user = new User(req.body);
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+  const user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: hashedPassword,
+  });
   await user
     .save()
     .then(() => res.send(user))
