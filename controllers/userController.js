d3f7eb240b1968ebaed022e65e45d97e82e6b57d
zakaria2105 <zelar903@gmail.com>
2022-01-18 22:49:31 +0100

    FSJS-68 login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,11 @@
 const User = require("../models/User");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
 const userController = {};
 
 userController.register = async (req, res) => {
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
   const user = new User({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -18,4 +18,12 @@ userController.register = async (req, res) => {
     .catch((err) => res.status(404).send(err.message));
 };
 
-module.exports = userController
+userController.login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(404).send("Email doesn't exist");
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(404).send("password wrong");
+  res.send("logged in");
+};
+
+module.exports = userController;
