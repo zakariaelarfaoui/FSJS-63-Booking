f86b898c87cf83d9fd6a9304af090464b9106094
zakaria2105 <zelar903@gmail.com>
2022-01-18 17:05:29 +0100

    FSJS-69 import auth routes to server

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 port = 5000;
 
+const authRoutes = require("./routes/auth")
+app.use("/", authRoutes);
+
+
+
 app.listen(port, () =>
-  console.log(`server running at http://localhost:${port}`)
+  console.log(`server running on http://localhost:${port}`)
 );
