860b0689b41dc22aac50c28ffddf4aa22ae2fee2
zakaria2105 <zelar903@gmail.com>
2022-01-20 19:03:54 +0100

    Remove all file from master branche

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-
-const userController = require("../controllers/userController")
-
-
-router.post("/register", userController.register);
-router.post("/login", userController.login);
-
-module.exports = router;
