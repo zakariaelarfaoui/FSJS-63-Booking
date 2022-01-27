ccd6932307e741e8278d12259b0b0dd536a3bcfd
zakaria2105 <zelar903@gmail.com>
2022-01-27 12:09:21 +0100

    feat: Add update client route
    FSJS-85

--- routes/client.route.js
+++ routes/client.route.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const clientController = require('../controllers/clientController.js')
 
 router.route("/create").post(clientController.create);
+router.route("/update/:id").post(clientController.update);
 
 module.exports = router;
