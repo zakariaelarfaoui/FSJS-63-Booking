f4dbb350b1420fb2027ae736b0ff51e5bf67244c
zakaria2105 <zelar903@gmail.com>
2022-01-27 12:09:50 +0100

    feat: Add update client validation
    FSJS-85

--- validation/clientValidation.js
+++ validation/clientValidation.js
@@ -9,4 +9,14 @@ const createValidation = (data) => {
   return schema.validate(data);
 };
 
-module.exports = createValidation ;
+const updateValidation = (data) => {
+    const schema = joi.object({
+      firstName: joi.string().required().min(3),
+      lastName: joi.string().required().min(3),
+      email: joi.string().required().email(),
+      role: joi.number().required(),
+      active: joi.boolean().required()
+    });
+    return schema.validate(data);
+}
+module.exports = {createValidation, updateValidation} ;
