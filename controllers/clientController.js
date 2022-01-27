ec07dde23c9d4d69c6ecde4d1cc9a00c50ac51b6
zakaria2105 <zelar903@gmail.com>
2022-01-27 14:13:45 +0100

    feat: add delete client function
    FSJS-84

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -54,4 +54,16 @@ const update = async (req, res) => {
   }
 };
 
-module.exports = { create, update };
+const deleteClient = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const client = await User.deleteOne({_id: id});
+    if(!client) return res.status(200).send("client not deleted")
+    return res.status(200).send("client deleted")
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = { create, update, deleteClient };
