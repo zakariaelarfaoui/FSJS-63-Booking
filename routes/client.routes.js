0e4f4665ea1d21b4e4b1bc75675fefd8c6ae2fe6
zakaria2105 <zelar903@gmail.com>
2022-02-01 17:01:33 +0100

    fix: fix client routes
    FSJS-80

--- routes/client.routes.js
+++ routes/client.routes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const clientController = require("../controllers/clientController.js");
 
 router.route("/create").post(clientController.create);
-router.route("/update/:id").post(clientController.update);
-router.route("/delete/:id").post(clientController.deleteClient);
+router.route("/update/:id").put(clientController.update);
+router.route("/delete/:id").delete(clientController.deleteClient);
 router.route("/").get(clientController.index);
 router.route("/:id").get(clientController.show);
 
