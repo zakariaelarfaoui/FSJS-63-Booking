37214539a057a7505bea8c9a5087c8e6f5dfc8b2
zakaria2105 <zelar903@gmail.com>
2022-02-01 09:52:03 +0100

    feat: Add update hotel routes
    - add update routes to hotel routes and index routes
    
    FSJS-92

--- routes/hotel.routes.js
+++ routes/hotel.routes.js
@@ -5,5 +5,8 @@ const upload = require('../middleware/multer');
 router
   .route("/create")
   .post(upload.array("images", 8), hotelController.createHotel);
+router
+  .route("/update/:id")
+  .put(upload.array("images", 8), hotelController.updateHotel);
 
 module.exports = router;
