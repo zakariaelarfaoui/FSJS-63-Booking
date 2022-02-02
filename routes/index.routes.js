fdd496362db3b4b2a84c123081d037e613e94137
zakaria2105 <zelar903@gmail.com>
2022-02-02 14:16:42 +0100

    feat: Add create room route to index route
    FSJS-97

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,11 @@
-const router = require('express').Router();
-const hotelRoutes = require('./hotel.routes')
-const clientRoutes = require('./client.routes')
+const router = require("express").Router();
+const hotelRoutes = require("./hotel.routes");
+const clientRoutes = require("./client.routes");
+const roomRoutes = require("./room.routes");
+const { route } = require("./room.routes");
 
 router.use("/hotels", hotelRoutes);
 router.use("/clients", clientRoutes);
+router.use("/rooms", roomRoutes);
 
 module.exports = router;
