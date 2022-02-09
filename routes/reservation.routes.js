2c63a307c358a1f08c8a5d0c53a3a51bf2a71c83
zakaria2105 <zelar903@gmail.com>
2022-02-09 21:47:10 +0100

    feat: Add  route to display available rooms to reservate
    FSJS-96

--- routes/reservation.routes.js
+++ routes/reservation.routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const reservationController = require("../Controllers/reservationController");
 
 router.route("/create").post(reservationController.createReservation);
+router.route("/available-rooms").get(reservationController.availableRooms);
 
-module.exports = router
+module.exports = router;
