e5ded13b0ec553827f99b5cb7b9c664cdf3b288d
zakaria2105 <zelar903@gmail.com>
2022-02-09 21:43:37 +0100

    feat: Add reservation route to index route
    FSJS-96

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,9 +3,11 @@ const hotelRoutes = require("./hotel.routes");
 const userRoutes = require("./user.routes");
 const authRoutes = require("./auth.routes");
 const roomRoutes = require("./room.routes");
+const reservationRoutes = require("./reservation.routes");
 const { verifyAuth, verifyPermission } = require("../middleware/auth");
 
 router.use("/", authRoutes);
+router.use("/reservations", reservationRoutes);
 router.use(verifyAuth);
 router.use("/hotels", verifyPermission("owner"), hotelRoutes);
 router.use("/users", verifyPermission("admin"), userRoutes);
