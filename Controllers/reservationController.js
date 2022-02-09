e0f13b313dd884584e89bb2e4639e75e9f5ea492
zakaria2105 <zelar903@gmail.com>
2022-02-09 02:46:29 +0100

    feat: Add create reservation function
    FSJS-96

---
+++ Controllers/reservationController.js
@@ -0,0 +1,49 @@
+const Reservation = require("../Models/Reservation");
+const reservationValidation = require("../Validation/reservationValidation");
+
+const createReservation = async (req, res) => {
+  try {
+    const result = reservationValidation(req.body);
+
+    if (result.error)
+      return res
+        .status(400)
+        .json({ error: true, message: result.error.message });
+    const reservationCheck = await Reservation.findOne({
+      $or: [
+        {
+          $and: [
+            {
+              from: { $lte: result.value.from },
+              to: { $gt: result.value.from },
+            },
+          ],
+        },
+        { from: { $gt: result.value.from, $lte: result.value.to } },
+      ],
+      roomId: result.value.roomId,
+    });
+    if (reservationCheck)
+      return res
+        .status(400)
+        .json({ error: true, message: "the room reserved in this date" });
+    const reservation = new Reservation(result.value);
+    await reservation.save((err, data) => {
+      if (err)
+        return res.status(400).json({
+          error: true,
+          message: err.message,
+        });
+      return res.status(200).json({
+        error: false,
+        message: "reservation created",
+        Reservation: reservation,
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: true, message: error.message });
+  }
+};
+
+module.exports = { createReservation };
