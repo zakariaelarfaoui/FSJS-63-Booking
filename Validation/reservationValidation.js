af983212ae766d1946a0d3d504770bb289b62d3a
zakaria2105 <zelar903@gmail.com>
2022-02-09 21:41:57 +0100

    fix: add validation to reservation date
    - validate Check in must be greater than today date
    - validate Checkout  must be greater Check in
    
    FSJS-96

--- Validation/reservationValidation.js
+++ Validation/reservationValidation.js
@@ -2,8 +2,12 @@ const joi = require("joi");
 
 const reservationValidation = (data) => {
   const schema = joi.object({
-    from: joi.date().required(),
-    to: joi.date().required(),
+    from: joi.date().required().greater("now").message({
+      "date.greater": "Check in must be greater than today date",
+    }),
+    to: joi.date().required().greater(joi.ref("from")).message({
+      "date.greater": "Checkout  must be greater Check in",
+    }),
     hotelId: joi.string().required(),
     roomId: joi.string().required(),
     clientId: joi.string().required(),
