833748a9e430a9467cccb355e13ff8cdc413d458
zakaria2105 <zelar903@gmail.com>
2022-02-09 21:37:15 +0100

    feat: Add  available room function
    -Create function to show all available room in a range
    
    FSJS-96

--- Controllers/reservationController.js
+++ Controllers/reservationController.js
@@ -1,10 +1,10 @@
 const Reservation = require("../Models/Reservation");
 const reservationValidation = require("../Validation/reservationValidation");
+const Room = require("../Models/Room");
 
 const createReservation = async (req, res) => {
   try {
     const result = reservationValidation(req.body);
-
     if (result.error)
       return res
         .status(400)
@@ -46,4 +46,36 @@ const createReservation = async (req, res) => {
   }
 };
 
-module.exports = { createReservation };
+const availableRooms = async (req, res) => {
+  try {
+    const { from, to } = req.query;
+    const filter = {
+      $or: [
+        {
+          $and: [
+            {
+              from: { $lte: from },
+              to: { $gt: from },
+            },
+          ],
+        },
+        { from: { $gt: from, $lte: to } },
+      ],
+    };
+    const reservations = await Reservation.find(filter, { _id: 1 }).populate(
+      "roomId",
+      "roomId"
+    );
+    const reservedRooms = reservations.map((reservation) => {
+      return reservation.roomId.id;
+    });
+    const rooms = await Room.find({ _id: { $nin: reservedRooms } });
+    const count = await Room.count({ _id: { $nin: reservedRooms } });
+    return res.json({ count: count, rooms: rooms });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: true, message: error.message });
+  }
+};
+
+module.exports = { createReservation, availableRooms };
