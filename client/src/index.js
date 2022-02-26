2ae1c375cdcecdad0c2e7ff8392211a669791109
zakaria2105 <zelar903@gmail.com>
2022-02-26 22:54:31 +0100

    init commit

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import reactDOM from "react-dom";
+
+import App from "./App";
+
+reactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
