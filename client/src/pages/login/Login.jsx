86dcdc58096e7646b5e42a34d9626babdd969bed
zakaria2105 <zelar903@gmail.com>
2022-03-02 19:05:08 +0100

    feat: Add login functionality
    FSJS-102

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+
+import axios from "../../api/axios";
+
+import useAuth from "../../hooks/useAuth";
+
+import LoginForm from "../../components/LoginForm";
+
 import "./style.css";
 
 function Login() {
+  const { setAuth } = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState({ email: "", password: "" });
+  const { email, password } = user;
+
+  const handelChange = (e) => {
+    setUser((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handelLogin = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const response = await axios.post('/login', user);
+      const { accessToken } = response.data;
+      const payload = jwt_decode(accessToken);
+      const { _id, email, firstName, lastName, role } = payload;
+
+      setAuth({ _id, email, firstName, lastName, role });
+      const from = location.state?.from?.pathname
+        ? location.state?.from?.pathname
+        : role === "admin" || role === "owner"
+        ? "/dashboard"
+        : "/";
+      navigate(from, { replace: true });
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+        setError(error.response.data.message);
+      } else {
+        console.log(error.message);
+      }
+    }
+  };
   return (
     <section className="login">
       <div className="login-left">
