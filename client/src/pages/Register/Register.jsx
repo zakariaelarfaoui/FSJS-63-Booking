de3b640628bdd4000610fda249bba5acf5d2c67f
zakaria2105 <zelar903@gmail.com>
2022-03-02 22:42:33 +0100

    feat: Add Register functionality
    FSJS-103

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,13 +1,64 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 
-import {Link} from "react-router-dom";
+import axios from "../../api/axios";
 
 import RegisterForm from "../../components/RegisterForm";
+import Popup from "../../components/Popup";
 
 import "./style.css";
 
 function Register() {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const { firstName, lastName, email, password } = user;
+
+  const handelChange = (e) => {
+    setUser((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  console.log(user);
+
+  const handelRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/register", user);
+      setSuccess(response.data.message);
+      setUser({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+        setError(error.response.data.message);
+      } else {
+        console.log(error.message);
+      }
+    }
+  };
   return (
     <section className="register">
+      {success && (
+        <Popup
+          faCircleCheck={faCircleCheck}
+          success={success}
+          setSuccess={setSuccess}
+        />
+      )}
+
       <div className="register-left">
         <h3 className="mt-5">Lorem, ipsum dolor.</h3>
         <p className="my-5">
@@ -18,8 +69,23 @@ function Register() {
         </Link>
       </div>
       <div className="register-right">
-        <h1 className="text-center mt-5">Sign up now</h1>
-        <RegisterForm />
+        <h1 className="text-center">Sign up now</h1>
+        {error && (
+          <div
+            className="alert alert-danger text-center w-75 mx-auto"
+            role="alert"
+          >
+            {error}
+          </div>
+        )}
+        <RegisterForm
+          firstName={firstName}
+          lastName={lastName}
+          email={email}
+          password={password}
+          handelChange={handelChange}
+          handelRegister={handelRegister}
+        />
       </div>
     </section>
   );
