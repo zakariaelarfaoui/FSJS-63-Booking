abd8552733ddb3bcd721dd581b4f895c5552f5ce
zakaria2105 <zelar903@gmail.com>
2022-03-03 18:57:02 +0100

    feat: add  modal component
    -add modal to display message when user submit form
    
    FSJS-105

--- client/src/pages/ForgotPassword/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword/ForgotPassword.jsx
@@ -5,8 +5,28 @@ import Modal from "../../components/Modal";
 
 const ForgotPassword = () => {
 
+  const props = {
+    title: "Check your email",
+    icon: success ? faCircleCheck : error ? faCircleInfo : null,
+    iconClassName: `d-block mt-3 mx-auto  ${
+      success ? "text-success" : "text-warning"
+    }`,
+    message: success ? success : error ? error : null,
+    headerClassName: `popup-head text-center text-black ${
+      success ? "bg-success text-white" : "bg-warning"
+    }`,
+    btnClassName: `btn ${
+      success ? "btn-outline-success" : "btn-outline-warning"
+    }`,
+    btnContent: "Close",
+    btnAction: "",
+    setSuccess,
+    setError,
+  };
+
   return (
     <>
+    {(success || error) && <Modal {...props} />}
       <section className="login">
         <div className="login-left">
           <h1 className="text-center mt-5">Forgot Password</h1>
