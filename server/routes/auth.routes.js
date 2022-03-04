92040861c042cc5c493610de166e2a1d6ce98181
EL ARFAOUI ZAKARIA <76808317+zakaria2105@users.noreply.github.com>
2022-03-04 15:45:52 +0100

    feat: :sparkles: add reset password route
    FSJS-76

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -6,6 +6,7 @@ router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/activate-account/:token", authController.activateAccount);
 router.post("/forgot-password", authController.forgotPassword);
+router.post("/reset-password/:token", authController.resetPassword);
 router.route("/refresh-token").get(authController.refreshToken);
 router.route("/logout").get(authController.logout);
 
