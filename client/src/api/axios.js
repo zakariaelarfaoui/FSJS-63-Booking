1254d75ffb7f9cd382960e8f9f88c846d5adfe7e
EL ARFAOUI ZAKARIA <76808317+zakaria2105@users.noreply.github.com>
2022-03-05 15:51:44 +0100

    feat: :passport_control: add axios private config
    add axios private to usit with request than need acces token

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,4 +2,10 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5000";
 
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
+
 export default axios.create({ baseURL: BASE_URL });
